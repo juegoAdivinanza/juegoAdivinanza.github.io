d57689847484a70504c1e1fb13d834eb62bd2c1a
juegoAdivinanza <74838654+juegoAdivinanza@users.noreply.github.com>
2020-11-21 16:03:22 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,1 +1,8 @@
 
+class  MiFooter  extiende  HTMLElement  {
+  connectedCallback ( )  {
+    esto . innerHTML  =  / * html * /
+      `Copyright y copia; 2020 Trinidad Garcia Brenda. » ;
+  }
+}
+customElements . define ( "mi-pie de página" ,  MiFooter ) ;
