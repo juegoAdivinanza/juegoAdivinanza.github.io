4615c632ac3e119ff7a583abfa0ba30e777ebdb0
juegoAdivinanza <nikigeminis3@gmail.com>
2020-11-24 16:37:34 -0600

    se modifico todo el codigo

--- cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class  MiFooter  extiende  HTMLElement  {
-  connectedCallback ( )  {
-    esto . innerHTML  =  / * html * /
-      `Copyright y copia; 2020 Trinidad Garcia Brenda. » ;
-  }
-}
-customElements . define ( "mi-pie de página" ,  MiFooter ) ;
--- js/CtrlSaludo.js
+++
@@ -1,9 +0,0 @@
-importar  {  valida  }  desde  "../lib/util.js" ;
-exportar  clase  CtrlSaludo  {
-  / ** @param { string } nombre * /
-  calculaSaludo ( nombre )  {
-    const  nombreSinEspacios  =  nombre ? nombre . recortar ( ) : "" ;
-    valida ( nombreSinEspacios ,  "Falta proporcionar el nombre." ) ;
-    volver  `de Hola $ { nombreSinEspacios } .` ;
-  }
-}
